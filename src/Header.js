d832860cffb83bc0b72c9b155b4d8940495d4921
beginerbeginer <miyabi6125@gmail.com>
2023-01-09 17:03:10 +0900

    [#1] 追加：ヘッダー

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <div className="title">掲示板</div>
+      <div className="menu-item">
+        <a href="#">スレッドをたてる</a>
+      </div>
+    </header>
+  );
+};

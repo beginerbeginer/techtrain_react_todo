3f261298251d25ce3d82d3f20b7581d8ee6f9ad9
beginerbeginer <miyabi6125@gmail.com>
2023-01-26 13:18:29 +0900

    [#2] 修正：スレッド一覧コンポーネント

---
+++ src/ThreadsListCotainer.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import axios from 'axios';
+import { Threads } from './Threads';
+const baseUrl = 'https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com';
+
+export const ThreadsListContainer = () => {
+  const [threads, setThreads] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get(`${baseUrl}/threads?offset=1`).then((res) => {
+      setThreads(res.data);
+    });
+  }, []);
+
+  return (
+    <main className="main">
+      <h1>新着スレッド</h1>
+      <ul className="thread_list">
+        <Threads threads={threads} />
+      </ul>
+    </main>
+  );
+};

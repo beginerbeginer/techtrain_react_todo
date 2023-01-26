2bfba0236b8b2daf95a28ba5666635f7e5879e63
beginerbeginer <miyabi6125@gmail.com>
2023-01-26 13:16:25 +0900

    [#2] 修正：ルーティング、コンポーネントの分離

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React from 'react';
-import axios from 'axios';
 import { Header } from './Header';
-import { Threads } from './Threads';
+import { ThreadsListContainer } from './ThreadsListCotainer';
+import { NewThreadForm } from './NewThreadForm';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-const baseURL = 'https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com';
-
 export const App = () => {
-  const [threads, setThreads] = React.useState([]);
-
-  React.useEffect(() => {
-    axios.get(`${baseURL}/threads?offset=1`).then((res) => {
-      setThreads(res.data);
-    });
-  }, []);
-
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <main className="main">
-        <h1>新着スレッド</h1>
-        <ul className="thread_list">
-          <Threads threads={threads} />
-        </ul>
-      </main>
-    </>
+      <Routes>
+        <Route path="/" element={<ThreadsListContainer />} />
+        <Route path="/thread/new" element={<NewThreadForm />} />
+      </Routes>
+    </BrowserRouter>
   );
 };

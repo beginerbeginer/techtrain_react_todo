575ec0953787976325fa6b6ca4def225cb2250f7
beginerbeginer <miyabi6125@gmail.com>
2023-01-30 17:07:19 +0900

    [#7] 追加：単体テスト

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 // toBeInTheDocument関数を使用するため
 import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { App } from './App';
 import { NewThreadForm } from './NewThreadForm';
@@ -43,5 +43,90 @@ describe('Thread new', () => {
     );
     const title = screen.getByText('スレッド新規作成');
     expect(title).toBeInTheDocument();
+  });
+});
+
+describe('<NewThreadForm />', () => {
+  test('renders the form', () => {
+    const { getByPlaceholderText, getByText } = render(
+      <BrowserRouter>
+        <NewThreadForm />
+      </BrowserRouter>,
+    );
+    const input = getByPlaceholderText('スレッドタイトル');
+    const button = getByText('作成');
+    expect(input).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+
+  test('validates the title length', async () => {
+    const { getByPlaceholderText, getByText } = render(
+      <BrowserRouter>
+        <NewThreadForm />
+      </BrowserRouter>,
+    );
+    const input = getByPlaceholderText('スレッドタイトル');
+    const button = getByText('作成');
+
+    fireEvent.change(input, { target: { value: 'a'.repeat(31) } });
+    await waitFor(() => {
+      expect(button).toBeDisabled();
+      expect(
+        getByText('タイトルの文字数は30文字以下にしてください'),
+      ).toBeInTheDocument();
+    });
+
+    fireEvent.change(input, { target: { value: ' '.repeat(31) } });
+    await waitFor(() => {
+      expect(button).toBeDisabled();
+      expect(
+        getByText('タイトルの文字数は30文字以下にしてください'),
+      ).toBeInTheDocument();
+    });
+  });
+
+  test('requires title input', async () => {
+    const { getByPlaceholderText, getByText } = render(
+      <BrowserRouter>
+        <NewThreadForm />
+      </BrowserRouter>,
+    );
+    const input = getByPlaceholderText('スレッドタイトル');
+    const button = getByText('作成');
+
+    fireEvent.click(button);
+    await waitFor(() => {
+      expect(button).toBeDisabled();
+      expect(
+        getByText('スレッドタイトルを入力してください'),
+      ).toBeInTheDocument();
+    });
+
+    fireEvent.change(input, { target: { value: ' '.repeat(30) } });
+    fireEvent.click(button);
+    await waitFor(() => {
+      expect(button).toBeDisabled();
+      expect(
+        getByText('スレッドタイトルを入力してください'),
+      ).toBeInTheDocument();
+    });
+
+    fireEvent.change(input, { target: { value: '　'.repeat(30) } });
+    fireEvent.click(button);
+    await waitFor(() => {
+      expect(button).toBeDisabled();
+      expect(
+        getByText('スレッドタイトルを入力してください'),
+      ).toBeInTheDocument();
+    });
+
+    fireEvent.change(input, { target: { value: '　 ' } });
+    fireEvent.click(button);
+    await waitFor(() => {
+      expect(button).toBeDisabled();
+      expect(
+        getByText('スレッドタイトルを入力してください'),
+      ).toBeInTheDocument();
+    });
   });
 });

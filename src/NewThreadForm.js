625de6748fe7f26edb1d60fa420b5e708625f4f8
beginerbeginer <miyabi6125@gmail.com>
2023-01-31 21:04:03 +0900

    [#7] 修正：バリデーションのロジックとメッセージを分離

--- src/NewThreadForm.js
+++ src/NewThreadForm.js
@@ -11,31 +11,30 @@ export const NewThreadForm = () => {
     handleSubmit,
     setError,
     formState: { errors },
-  } = useForm({
-    mode: 'onChange',
-  });
+  } = useForm();
+
+  // タイトル入力時に実行されるバリデーション
+  const onChangeTitle = useCallback((e) => {
+    const titleName = e.target.value;
+    // 30文字のバリデーションは暫定対応。仕様確定後に修正。
+    if (titleName.length > 30) {
+      setError('title', {
+        type: 'maxLength',
+      });
+    } else {
+      setError('title', {
+        type: '',
+      });
+    }
+  }, []);
 
   // 送信時に実行されるバリデーション＆例外処理
   const onSubmit = useCallback(
     async (data) => {
-      let type = '';
-      let errorMessage = '';
-
       if (!data.title.trim()) {
-        type = 'required';
-        errorMessage = 'スレッドタイトルを入力してください';
-      }
-      // 30文字のバリデーションは暫定対応。仕様確定後に修正。
-      if (data.title.length > 30) {
-        type = 'maxLength';
-        errorMessage = 'タイトルの文字数は30文字以下にしてください';
-      }
-      if (errorMessage) {
         setError('title', {
-          type,
-          message: errorMessage,
+          type: 'required',
         });
-        return;
       }
       try {
         const response = await axios.post(`${baseUrl}/threads`, {
@@ -45,16 +44,14 @@ export const NewThreadForm = () => {
           navigate('/');
         }
       } catch (err) {
+        let errorMessage = '';
         if (err.response) {
           errorMessage = err.response.data.ErrorMessageJP;
+          console.error(errorMessage);
         } else {
           errorMessage = 'Unknown Error';
-          console.error(err);
+          console.error(errorMessage);
         }
-        setError('title', {
-          type: 'Unknown Error',
-          message: errorMessage,
-        });
       }
     },
     [navigate],
@@ -67,11 +64,15 @@ export const NewThreadForm = () => {
         <input
           name="title"
           {...register('title', {
+            required: true,
             maxLength: 30,
           })}
           placeholder="スレッドタイトル"
+          onChange={onChangeTitle}
         />
-        {errors?.title?.type && <p>{errors.title.message}</p>}
+        {errors?.title?.type === 'required' && (
+          <p>スレッドタイトルを入力してください</p>
+        )}
         {errors?.title?.type === 'maxLength' && (
           <p>タイトルの文字数は30文字以下にしてください</p>
         )}

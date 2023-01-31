751e9bbe2fdb8546a90a62df89341a1014216f68
beginerbeginer <miyabi6125@gmail.com>
2023-01-31 13:57:56 +0900

    [#7] 修正：フォームの実装をreact-hook-formに変更

--- src/NewThreadForm.js
+++ src/NewThreadForm.js
@@ -1,35 +1,33 @@
 import React, { useState, useCallback } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 const baseUrl = process.env.REACT_APP_BASEURL;
 
 export const NewThreadForm = () => {
   const navigate = useNavigate();
-  const [title, setTitle] = useState('');
   const [error, setError] = useState(null);
+  const { register, handleSubmit } = useForm();
 
   // 文字を入力する時に実行されるバリデーション
-  const onChangeTitle = useCallback((e) => {
-    const titleName = e.target.value;
-    if (titleName.length > 30) {
+  const validate = (value) => {
+    if (value.length > 30) {
       setError('タイトルの文字数は30文字以下にしてください'); // 30文字のバリデーションは暫定対応。仕様確定後に修正。
     } else {
       setError(null);
     }
-    setTitle(titleName);
-  }, []);
+  };
 
   // 送信時に実行されるバリデーション＆例外処理
   const onSubmit = useCallback(
-    async (e) => {
-      e.preventDefault();
-      if (!title.trim()) {
+    async (data) => {
+      if (!data.title.trim()) {
         setError('スレッドタイトルを入力してください');
         return;
       }
       try {
         const response = await axios.post(`${baseUrl}/threads`, {
-          title: title.trim(),
+          title: data.title.trim(),
         });
         if (response.status === 200) {
           navigate('/');
@@ -45,16 +43,19 @@ export const NewThreadForm = () => {
         setError(errorMessage);
       }
     },
-    [title, navigate],
+    [navigate],
   );
 
   return (
     <main className="main">
       <h1>スレッド新規作成</h1>
-      <form onSubmit={onSubmit} className="form">
+      <form onSubmit={handleSubmit(onSubmit)} className="form">
         <input
-          value={title}
-          onChange={onChangeTitle}
+          name="title"
+          ref={register()}
+          onChange={(e) => {
+            validate(e.target.value);
+          }}
           placeholder="スレッドタイトル"
         />
         {error && <p className="error">{error}</p>}

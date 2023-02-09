8d7017fa80fa36da08fd6bf4796a41684691d590
beginerbeginer <miyabi6125@gmail.com>
2023-02-09 19:36:43 +0900

    [#4] 修正：react-hook-formで書き換え。送信時と入力時のバリデーションも追加

--- src/CommentForm.js
+++ src/CommentForm.js
@@ -1,18 +1,43 @@
-import React, { useState } from 'react';
+import React, { useCallback } from 'react';
 import { PropTypes } from 'prop-types';
 import axios from 'axios';
+import { useForm } from 'react-hook-form';
 const baseUrl = process.env.REACT_APP_BASEURL;
 
 export const CommentForm = ({ threadId }) => {
-  const [posts, setPosts] = useState('');
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  // タイトル入力時に実行されるバリデーション
+  const onChangePost = useCallback((e) => {
+    const texts = e.target.value;
+    // 140文字のバリデーションは暫定対応。仕様確定後に修正。
+    if (texts.length > 140) {
+      setError('posts', {
+        type: 'maxLength',
+      });
+    } else {
+      setError('posts', {
+        type: '',
+      });
+    }
+  }, []);
+
+  const onSubmit = useCallback(async (data) => {
+    if (!data.posts.trim()) {
+      setError('posts', {
+        type: 'required',
+      });
+    }
     try {
       const response = await axios.post(
         `${baseUrl}/threads/${threadId}/posts`,
         {
-          post: posts,
+          posts: data.posts.trim(),
         },
         {
           headers: {
@@ -24,23 +49,42 @@ export const CommentForm = ({ threadId }) => {
       if (response.status === 200) {
         window.location.reload();
       }
-    } catch (error) {
-      console.error(error);
+    } catch (err) {
+      let errorMessage = '';
+      if (err.response) {
+        errorMessage = err.response.data.ErrorMessageJP;
+        console.error(errorMessage);
+      } else {
+        errorMessage = 'Unknown Error';
+        console.error(errorMessage);
+      }
     }
-  };
+  }, []);
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <textarea
+        name="posts"
+        {...register('posts', {
+          required: true,
+          maxLength: 140,
+        })}
         placeholder="投稿しよう!"
-        value={posts}
-        onChange={(e) => setPosts(e.target.value)}
+        onChange={onChangePost}
       />
       <div>
-        <button type="submit" className="button">
+        <button
+          type="submit"
+          className="button"
+          disabled={Boolean(errors?.posts?.type)}
+        >
           投稿
         </button>
       </div>
+      {errors?.posts?.type === 'required' && <p>投稿を入力してください</p>}
+      {errors?.posts?.type === 'maxLength' && (
+        <p>文字数は140文字以下にしてください</p>
+      )}
     </form>
   );
 };
